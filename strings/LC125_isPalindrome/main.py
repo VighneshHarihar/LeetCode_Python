ae91d69bf51198eb00befb24e049936f20dff831
vighnesh <hariharvighnesh@gmail.com>
2020-09-04 12:19:18 +0530

    4 sept

--- strings/LC125_isPalindrome/main.py
+++ strings/LC125_isPalindrome/main.py
@@ -1,2 +1,15 @@
 class Solution:
     def isPalindrome(self, s):
+        import re
+        s = re.sub(r'[\W_]', '', s).lower()
+
+        # Solution 1
+        front = 0
+        back = len(s)-1
+
+        while front < back:
+            if(s[front] != s[back]):
+                return False
+            front += 1
+            back -= 1
+        return True
