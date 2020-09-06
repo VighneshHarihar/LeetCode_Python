75c8ec818be9c708c3854b3e10e87c93094d2567
vighnesh <hariharvighnesh@gmail.com>
2020-09-06 18:05:17 +0530

    6 Sept

--- strings/LC5_longestPalindrome/main.py
+++ strings/LC5_longestPalindrome/main.py
@@ -6,3 +6,22 @@
 
 class Solution:
     def longestPalindrome(self, s):
+        res = ""
+        for i in range(len(s)):
+            # print("for", i, ": ")
+            current = self.findS(s, i-1, i+1)
+            in_bet = self.findS(s, i, i+1)
+            res = max(res, current, in_bet, key=len)
+            # print("current", current, " in_bet:", in_bet, " res", res)
+        return res
+
+    def findS(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return s[left+1:right]
+
+
+# obj = Solution()
+# s = "madam"
+# print("final:"+obj.longestPalindrome(s))
